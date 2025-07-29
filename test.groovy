7f1a44f67656c4d1f7a21d7e217a3dcd9190c001
Shishupal <shishupalsingh1137@gmail.com>
2025-07-30 00:07:33 +0530

    email added pass testcase

--- test.groovy
+++ test.groovy
@@ -21,4 +21,24 @@ pipeline {
             }
         }
     }
+
+    post {
+        success {
+            emailext (
+                to: 'rajabhaiya1137@gmail.com',
+                subject: "✅ Build Passed: ${env.JOB_NAME} #${env.BUILD_NUMBER}",
+                body: """
+                Hello Raja Bhaiya,
+
+                ✅ Your Jenkins build *${env.JOB_NAME}* ran successfully!
+
+                🔢 Build Number: ${env.BUILD_NUMBER}
+                🔗 View Details: ${env.BUILD_URL}
+
+                Regards,  
+                Jenkins CI
+                """
+            )
+        }
+    }
 }
